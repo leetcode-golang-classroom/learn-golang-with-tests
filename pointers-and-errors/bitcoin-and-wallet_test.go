dccc5ae582d3a0de341364ddde68de2f94c79d72
gson Liang <yuanyu90221@gmail.com>
2025-06-07 18:07:19 +0800

    ✨ (bitcoin-wallet): add withdraw test

--- pointers-and-errors/bitcoin-and-wallet_test.go
+++ pointers-and-errors/bitcoin-and-wallet_test.go
@@ -4,12 +4,28 @@ import "testing"
 
 // Bitcoin Wallet
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
 
-	wallet.Deposit(10)
-	got := wallet.Balance()
-	want := Bitcoin(10)
-	if got != want {
-		t.Errorf("got %v, want %v", got, want)
-	}
+		wallet.Deposit(10)
+		got := wallet.Balance()
+		want := Bitcoin(10)
+		if got != want {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	})
+
+	t.Run("withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+
+		wallet.Withdraw(Bitcoin(10))
+
+		got := wallet.Balance()
+
+		want := Bitcoin(10)
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
 }
