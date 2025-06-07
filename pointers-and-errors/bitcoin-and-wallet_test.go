2cf0140efca182819bf049a1366598bd29146fe9
gson Liang <yuanyu90221@gmail.com>
2025-06-07 20:25:00 +0800

    ♻️ (bitcoin-wallet): add assertBalance for better check

--- pointers-and-errors/bitcoin-and-wallet_test.go
+++ pointers-and-errors/bitcoin-and-wallet_test.go
@@ -4,28 +4,29 @@ import "testing"
 
 // Bitcoin Wallet
 func TestWallet(t *testing.T) {
+
+	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
+		t.Helper()
+		got := wallet.Balance()
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	}
+
 	t.Run("deposit", func(t *testing.T) {
 		wallet := Wallet{}
 
 		wallet.Deposit(10)
-		got := wallet.Balance()
 		want := Bitcoin(10)
-		if got != want {
-			t.Errorf("got %v, want %v", got, want)
-		}
+		assertBalance(t, wallet, want)
 	})
 
 	t.Run("withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
 
 		wallet.Withdraw(Bitcoin(10))
-
-		got := wallet.Balance()
-
 		want := Bitcoin(10)
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertBalance(t, wallet, want)
 	})
 }
