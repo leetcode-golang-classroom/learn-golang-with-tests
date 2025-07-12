be4efb607d16b5c3851e59541d0bd29012f9e52a
gson Liang <yuanyu90221@gmail.com>
2025-07-12 23:22:29 +0800

    ✨ (roman_numerals): add fourth test and refactor

--- property-base-test/roman_number.go
+++ property-base-test/roman_number.go
@@ -5,6 +5,10 @@ import "strings"
 func ConvertToRoman(arabic int) string {
 	var result strings.Builder
 	for i := arabic; i > 0; i-- {
+		if arabic == 4 {
+			result.WriteString("IV")
+			break
+		}
 		result.WriteString("I")
 	}
 	return result.String()
