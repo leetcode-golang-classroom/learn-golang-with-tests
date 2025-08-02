e20950c1dcd2404f70c7496a8b7b1c542ebcceee
gson Liang <yuanyu90221@gmail.com>
2025-08-03 01:10:42 +0800

    ✨ (clockface): add secondsInRadians implementation

--- clockface/clockface.go
+++ clockface/clockface.go
@@ -18,5 +18,13 @@ func SecondHand(t time.Time) Point {
 }
 
 func secondsInRadians(t time.Time) float64 {
-	return math.Pi
+	return (math.Pi / (30 / (float64(t.Second()))))
+}
+
+func simpleTime(hours, minutes, seconds int) time.Time {
+	return time.Date(312, time.October, 28, hours, minutes, seconds, 0, time.UTC)
+}
+
+func testName(t time.Time) string {
+	return t.Format("15:04:05")
 }
