1a7f45ef4a9d99b0fd5c7a9d962f1cd0adfb4676
gson Liang <yuanyu90221@gmail.com>
2025-08-03 18:19:48 +0800

    ✨ (clockface): add svg struct for parser

--- clockface_sample/clockface/clockface_acceptance_test.go
+++ clockface_sample/clockface/clockface_acceptance_test.go
@@ -1,6 +1,8 @@
 package clockface_test
 
 import (
+	"bytes"
+	"encoding/xml"
 	"strings"
 	"testing"
 	"time"
@@ -8,18 +10,50 @@ import (
 	clockface "github.com/leetcode-golang-classroom/learn-golang-with-tests/clockface_sample/clockface"
 )
 
+type Svg struct {
+	XMLName xml.Name `xml:"svg"`
+	Text    string   `xml:",chardata"`
+	Xmlns   string   `xml:"xmlns,attr"`
+	Width   string   `xml:"width,attr"`
+	Height  string   `xml:"height,attr"`
+	ViewBox string   `xml:"viewBox,attr"`
+	Version string   `xml:"version,attr"`
+	Circle  struct {
+		Text  string `xml:",chardata"`
+		Cx    string `xml:"cx,attr"`
+		Cy    string `xml:"cy,attr"`
+		R     string `xml:"r,attr"`
+		Style string `xml:"style,attr"`
+	} `xml:"circle"`
+	Line []struct {
+		Text  string `xml:",chardata"`
+		X1    string `xml:"x1,attr"`
+		Y1    string `xml:"y1,attr"`
+		X2    string `xml:"x2,attr"`
+		Y2    string `xml:"y2,attr"`
+		Style string `xml:"style,attr"`
+	} `xml:"line"`
+}
+
 func TestSVGWriterAtMidnight(t *testing.T) {
 	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
 
-	var b strings.Builder
+	b := bytes.Buffer{}
 	clockface.SVGWriter(&b, tm)
-	got := b.String()
+	svg := Svg{}
 
-	want := `<line x1="150" y1="150" x2="150.000" y2="60.000"`
+	xml.Unmarshal(b.Bytes(), &svg)
 
-	if !strings.Contains(got, want) {
-		t.Errorf("Expected to find the second hand %v, in the SVG output %v", want, got)
+	x2 := "150.000"
+	y2 := "60.000"
+
+	for _, line := range svg.Line {
+		if line.X2 == x2 && line.Y2 == y2 {
+			return
+		}
 	}
+
+	t.Errorf("Expected to find the second hand with x2 of %v and y2 of %v, in the SVG output %v", x2, y2, b.String())
 }
 
 func TestSVGWriterAt30Seconds(t *testing.T) {
