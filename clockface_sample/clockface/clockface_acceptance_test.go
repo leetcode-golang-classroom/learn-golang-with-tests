ec2fa0a8288e055f8636c0c3eb51ba6e257108a0
gson Liang <yuanyu90221@gmail.com>
2025-08-03 22:18:34 +0800

    ✨ (clockface): add minutesInRadians implementation

--- clockface_sample/clockface/clockface_acceptance_test.go
+++ clockface_sample/clockface/clockface_acceptance_test.go
@@ -100,3 +100,29 @@ func TestSecondHandAt30Seconds(t *testing.T) {
 		t.Errorf("got %v, wanted %v", got, want)
 	}
 }
+
+// func TestSVGWriterMinuteHand(t *testing.T) {
+// 	cases := []struct {
+// 		time time.Time
+// 		line Line
+// 	}{
+// 		{
+// 			simpleTime(0, 0, 0),
+// 			Line{150, 150, 150, 70},
+// 		},
+// 	}
+
+// 	for _, c := range cases {
+// 		t.Run(testName(c.time), func(t *testing.T) {
+// 			b := bytes.Buffer{}
+// 			clockface.SVGWriter(&b, c.time)
+
+// 			svg := SVG{}
+// 			xml.Unmarshal(b.Bytes(), &svg)
+
+// 			if !containsLine(c.line, svg.Line) {
+// 				t.Errorf("Expected to find the minute hand line %+v, in the SVG lines %+v", c.line, svg.Line)
+// 			}
+// 		})
+// 	}
+// }
--- clockface_sample/clockface/clockface_test.go
+++ clockface_sample/clockface/clockface_test.go
@@ -62,3 +62,22 @@ func roughlyEqualPoint(a, b Point) bool {
 	return roughlyEqualFloat64(a.X, b.X) &&
 		roughlyEqualFloat64(a.Y, b.Y)
 }
+
+func TestMinutesInRadians(t *testing.T) {
+	cases := []struct {
+		time  time.Time
+		angle float64
+	}{
+		{simpleTime(0, 30, 0), math.Pi},
+		{simpleTime(0, 0, 7), 7 * (math.Pi / (30 * 60))},
+	}
+
+	for _, c := range cases {
+		t.Run(testName(c.time), func(t *testing.T) {
+			got := minutesInRadians(c.time)
+			if got != c.angle {
+				t.Fatalf("Wanted %v radians, but got %v", c.angle, got)
+			}
+		})
+	}
+}
